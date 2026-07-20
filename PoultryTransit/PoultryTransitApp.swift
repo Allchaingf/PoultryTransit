e6d26e0df2c0615daf5d16b5baf30c95b8c5ee1c
Allchaingf <ulymarina0909@gmail.com>
2026-07-20 11:11:21 +0200

    Initial Commit

---
+++ PoultryTransit/PoultryTransitApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoultryTransitApp.swift
+//  PoultryTransit
+//
+//  Created by Andres Ivanov on 20/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct PoultryTransitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
